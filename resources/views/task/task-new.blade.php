428ae61feb1455268aa549bc455350c93734db9d
TheCrystalH3ro <matusnema@gmail.com>
2021-12-01 14:10:07 +0100

    Create new task form

--- resources/views/task/task-new.blade.php
+++ resources/views/task/task-new.blade.php
@@ -1,1 +1,19 @@
 @extends('task.task-master')
+
+@section('task-heading')
+    
+    @include('components.task-categories')
+
+@endsection
+
+@section('task-top-options')
+    
+    @include('components.task-visibility')
+
+@endsection
+
+@section('task-footer')
+    
+    <button type="submit" class="waves-effect waves-light btn teal lighten-2"> {{ __('tasks.createButton') }} </button>
+
+@endsection
