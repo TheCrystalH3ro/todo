05aeac235b74c775c150516f82014417ccaab4e1
TheCrystalH3ro <matusnema@gmail.com>
2021-12-02 11:28:37 +0100

    Task Visibility Modal

--- resources/views/components/task-visibility.blade.php
+++ resources/views/components/task-visibility.blade.php
@@ -4,7 +4,7 @@
 
 <div id="visibility" class="modal">
     <div class="modal-content">
-      <h4>{{ __('tasks.visibility') }}:
+        <h4>{{ __('tasks.visibility') }}:
         {{-- SHOW TASK'S VISBILITY --}}
         @isset($visibility)            
             <i>{{ __('tasks.' . $visibility) }}</i>
@@ -14,34 +14,34 @@
         @empty($visibility)
             <i>{{ __('tasks.public') }}</i>
         @endempty 
-    </h4>
-      <p>{{ __('tasks.visibility-info') }}</p>
-      <div class="switch">
-        <label>
-          {{ __('tasks.private') }}
-          <input type="checkbox" name="visibility"
-            {{-- INSERT TASK'S VISIBILITY VALUE --}}
-            @isset($task)
-                {{ $task->visibility ? 'checked' : '' }}
-            @endisset
+        </h4>
+        <p>{{ __('tasks.visibility-info') }}</p>
+        <div class="switch">
+            <label>
+            {{ __('tasks.private') }}
+            <input type="checkbox" name="visibility"
+                {{-- INSERT TASK'S VISIBILITY VALUE --}}
+                @isset($task)
+                    {{ $task->visibility ? 'checked' : '' }}
+                @endisset
 
-            {{-- SET DEFAULT VALUE IF TASK CREATION --}}
-            @empty($task)                
-                checked
-            @endempty
-           >
-          <span class="lever teal lighten-2"></span>
-          {{ __('tasks.public') }}
-        </label>
-      </div>
-      <ul>
-        <li><b>{{ __('tasks.public') }}</b> - {{ __('tasks.visibility-info-public') }}</li>
-        <li><b>{{ __('tasks.private') }}</b> - {{__('tasks.visibility-info-private')}}</li>
-      </ul>
+                {{-- SET DEFAULT VALUE IF TASK CREATION --}}
+                @empty($task)                
+                    checked
+                @endempty
+            >
+            <span class="lever teal lighten-2"></span>
+            {{ __('tasks.public') }}
+            </label>
+        </div>
+        <ul>
+            <li><b>{{ __('tasks.public') }}</b> - {{ __('tasks.visibility-info-public') }}</li>
+            <li><b>{{ __('tasks.private') }}</b> - {{__('tasks.visibility-info-private')}}</li>
+        </ul>
     </div>
     <div class="modal-footer">
         <a class="modal-close waves-effect waves-light btn modal-trigger teal lighten-2" href="#!">
             <span>{{ __('general.done') }}</span>
         </a>
     </div>
-  </div>
+</div>
